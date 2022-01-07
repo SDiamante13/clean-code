175a2539f3034446361435cd0fe3b34f6cdc1695
Steven Diamante <stevendiamante@steven-diamante.lan>
2022-01-07 14:49:49 -0500

    Add AssertJ. Refactor Notmal Item tests.

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
@@ -4,41 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class GildedRoseADefaultItemTest {
-	/**
-	 * Method to test the variation in quality of the item for the non expired
-	 * Item.
-	 * 
-	 * The quality should decrease by 1 when the item is not expired
-	 * and sell in should decrease by 1.
-	 * 
-	 */
-	@Test
-	public void testUpdateQualityDefault1() {
-		Item item = new Item("DEFAULT_ITEM", 15, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("DEFAULT_ITEM", app.items[0].name);
-		assertEquals(14, app.items[0].sellIn);
-		assertEquals(2, app.items[0].quality);
-	}
+import static org.assertj.core.api.Assertions.*;
 
-	/**
-	 * Method to test the variation in quality of the item for the non expired
-	 * Item.
-	 * 
-	 * The quality should decrease by 2 when the item is expired(Sell in  < 0) and sell in should decrease by 1.
-	 * 
-	 */
-	@Test
-	public void testUpdateQualityForExpiredItem() {
-		Item item = new Item("DEFAULT_ITEM", -1, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("DEFAULT_ITEM", app.items[0].name);
-		assertEquals(-2, app.items[0].sellIn);
-		assertEquals(1, app.items[0].quality);
-	}
+class GildedRoseADefaultItemTest {
+
+    @Test
+    void updateQuality_qualityAndSellInShouldDecreaseByOne_whenItemIsNotExpired() {
+        GildedRose app = new GildedRose(new Item[]{
+                new Item("DEFAULT_ITEM", 15, 3)
+        });
+
+        app.updateQuality();
+
+        assertThat(app.items)
+                .extracting("name", "sellIn", "quality")
+                .contains(tuple("DEFAULT_ITEM", 14, 2));
+    }
+
+    @Test
+    void updateQuality_qualityShouldDecreaseByTwo_whenItemIsExpired() {
+        GildedRose app = new GildedRose(new Item[]{
+                new Item("DEFAULT_ITEM", -1, 3)
+        });
+
+        app.updateQuality();
+
+        assertThat(app.items)
+                .extracting("name", "sellIn", "quality")
+                .contains(tuple("DEFAULT_ITEM", -2, 1));
+    }
 }
