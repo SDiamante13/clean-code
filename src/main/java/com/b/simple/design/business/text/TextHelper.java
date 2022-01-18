6b4617bdc8bc2ee15dacf8f1014daa5dcebe8378
Steven Diamante <stevendiamante13@gmail.com>
2022-01-17 22:09:10 -0500

    Update TextHelper code

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -3,10 +3,22 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+		int length = str.length();
+		if (length < 2) return str;
+		char lastChar = str.charAt(length - 1);
+		char secondToLastChar = str.charAt(length - 2);
+
+		return str.substring(0, length - 2) + lastChar + secondToLastChar;
 	}
 
 	public String truncateAInFirst2Positions(String str) {
-		return null;
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (i >= 2 || str.charAt(i) != 'A') {
+				stringBuilder.append(str.charAt(i));
+			}
+		}
+		return stringBuilder.toString();
 	}
 }
